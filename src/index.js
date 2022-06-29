3e3ac8d4ba6137c0a175cef0ea0eed0dc9231a48
Samantha Wu <samanthayywu@gmail.com>
2022-06-29 21:09:53 +0100

    the wise man

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import chalk from "chalk";
+
+console.log(chalk.cyan.underline("The Wise Man"));
+
+console.log(
+  chalk.yellow("A wise man used to give advice to anyone who approached him.")
+);
+
+console.log(
+  chalk.magenta(
+    "Over the years, he noticed that people had been complaining about the same problems to him. "
+  )
+);
+console.log(
+  chalk.green("One day, he told a joke, which made everyone laugh out loud.")
+);
+console.log(
+  chalk.red(
+    "He told them the same joke after a few minutes, and only a few of them smiled."
+  )
+);
+console.log(
+  chalk.blue("The third time he told the same joke, no one laughed.")
+);
+console.log(
+  chalk.rgb(
+    100,
+    50,
+    67
+  )("He smiled and said, “You can’t laugh at the same joke over and over.")
+);
+console.log(
+  chalk.rgb(
+    50,
+    150,
+    80
+  )("So why are you always crying about the same problem?”")
+);
