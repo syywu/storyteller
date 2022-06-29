869e167b55d20af3ee49bce420df9ad7cfdaf299
Samantha Wu <samanthayywu@gmail.com>
2022-06-29 21:31:51 +0100

    story func

--- src/index.js
+++ src/index.js
@@ -1,42 +1,55 @@
 import chalk from "chalk";
 
-function sleep(ms) {
+async function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-console.log(chalk.cyan.underline("The Wise Man"));
-sleep(1000);
-console.log(
-  chalk.yellow("A wise man used to give advice to anyone who approached him.")
-);
+await story();
 
-console.log(
-  chalk.magenta(
-    "Over the years, he noticed that people had been complaining about the same problems to him. "
-  )
-);
-console.log(
-  chalk.bgGreen("One day, he told a joke, which made everyone laugh out loud.")
-);
-console.log(
-  chalk.bgRed(
-    "He told them the same joke after a few minutes, and only a few of them smiled."
-  )
-);
-console.log(
-  chalk.bgBlue("The third time he told the same joke, no one laughed.")
-);
-console.log(
-  chalk.rgb(
-    100,
-    50,
-    67
-  )("He smiled and said, “You can’t laugh at the same joke over and over.")
-);
-console.log(
-  chalk.rgb(
-    50,
-    150,
-    80
-  )("So why are you always crying about the same problem?”")
-);
+async function story() {
+  console.log(chalk.cyan.underline("The Wise Man"));
+  await sleep(2000);
+  console.log(
+    chalk.yellow("A wise man used to give advice to anyone who approached him.")
+  );
+  await sleep(2000);
+  console.log(
+    chalk.magenta(
+      "Over the years, he noticed that people had been complaining about the same problems to him. "
+    )
+  );
+  await sleep(2000);
+  console.log(
+    chalk.bgGreen(
+      "One day, he told a joke, which made everyone laugh out loud."
+    )
+  );
+  await sleep(2000);
+  console.log(
+    chalk.bgRed(
+      "He told them the same joke after a few minutes, and only a few of them smiled."
+    )
+  );
+  await sleep(2000);
+  console.log(
+    chalk.bgBlue("The third time he told the same joke, no one laughed.")
+  );
+  await sleep(2000);
+  console.log(
+    chalk.rgb(
+      100,
+      50,
+      67
+    )("He smiled and said, “You can’t laugh at the same joke over and over.")
+  );
+  await sleep(2000);
+  console.log(
+    chalk.rgb(
+      50,
+      150,
+      80
+    )("So why are you always crying about the same problem?”")
+  );
+  await sleep(2000);
+  process.exit(1);
+}
